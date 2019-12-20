601b4820202c1b5ce0c2f46c53e42aa7f4436575
Brian Burns <bdburns6389@gmail.com>
2019-12-19 20:59:47 -0600

    MobX Refactoring

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -89,8 +89,6 @@ const App = () => {
         <ActivityDashboard
           activities={activityStore.activities}
           selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
           createActivity={handleCreateActivity}
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,20 +5,29 @@ import agent from "../api/agent";
 
 class ActivityStore {
   @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | undefined;
   @observable loadingInitial = false;
+  @observable editMode = false;
 
-  @action loadActivities = () => {
+  @action loadActivities = async () => {
     // Sets loading wheel to show while we are retrieving our activities.
     this.loadingInitial = true;
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach(activity => {
+        activity.date = activity.date.split(".")[0];
+        this.activities.push(activity);
+      });
+      this.loadingInitial = false;
+    } catch (error) {
+      console.log(error);
+      this.loadingInitial = false;
+    }
+  };
 
-    agent.Activities.list()
-      .then(activities => {
-        activities.forEach(activity => {
-          activity.date = activity.date.split(".")[0];
-          this.activities.push(activity);
-        });
-      })
-      .finally(() => (this.loadingInitial = false));
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find(c => c.id === id);
+    this.editMode = false;
   };
 }
 
