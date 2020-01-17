de354eb65551b5d9999a89c5f836d82194d2bed8
Brian Burns <bdburns6389@gmail.com>
2020-01-16 21:57:43 -0600

    End of Section 9

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import ActivityStore from "../../../app/stores/activityStore";
@@ -76,62 +76,67 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
 
   return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          placeholder="Title"
-          value={activity.title}
-          onChange={handleInputChange}
-          name="title"
-        />
-        <Form.TextArea
-          rows={2}
-          placeholder="Description"
-          value={activity.description}
-          onChange={handleInputChange}
-          name="description"
-        />
-        <Form.Input
-          placeholder="Category"
-          value={activity.category}
-          onChange={handleInputChange}
-          name="category"
-        />
-        <Form.Input
-          type="datetime-local"
-          placeholder="Date"
-          value={activity.date}
-          onChange={handleInputChange}
-          name="date"
-        />
-        <Form.Input
-          placeholder="City"
-          value={activity.city}
-          onChange={handleInputChange}
-          name="city"
-        />
-        <Form.Input
-          placeholder="Venue"
-          value={activity.venue}
-          onChange={handleInputChange}
-          name="venue"
-        />
-        <Button
-          loading={submitting}
-          floated="right"
-          positive
-          type="submit"
-          content="Submit"
-          inverted
-          color="green"
-        />
-        <Button
-          floated="right"
-          content="Cancel"
-          onClick={() => history.push("/activities")}
-        />
-      </Form>
-    </Segment>
+    // Can add 'centered' as attribute to <Grid> to center everything on page.
+    <Grid>
+      <Grid.Column width={10}>
+        <Segment clearing>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              placeholder="Title"
+              value={activity.title}
+              onChange={handleInputChange}
+              name="title"
+            />
+            <Form.TextArea
+              rows={2}
+              placeholder="Description"
+              value={activity.description}
+              onChange={handleInputChange}
+              name="description"
+            />
+            <Form.Input
+              placeholder="Category"
+              value={activity.category}
+              onChange={handleInputChange}
+              name="category"
+            />
+            <Form.Input
+              type="datetime-local"
+              placeholder="Date"
+              value={activity.date}
+              onChange={handleInputChange}
+              name="date"
+            />
+            <Form.Input
+              placeholder="City"
+              value={activity.city}
+              onChange={handleInputChange}
+              name="city"
+            />
+            <Form.Input
+              placeholder="Venue"
+              value={activity.venue}
+              onChange={handleInputChange}
+              name="venue"
+            />
+            <Button
+              loading={submitting}
+              floated="right"
+              positive
+              type="submit"
+              content="Submit"
+              inverted
+              color="green"
+            />
+            <Button
+              floated="right"
+              content="Cancel"
+              onClick={() => history.push("/activities")}
+            />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
