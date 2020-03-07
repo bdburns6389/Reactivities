9d7c3915f0d921fc6b28a7a71f2ea48681997929
Brian Burns <bdburns6389@gmail.com>
2020-03-06 21:53:50 -0600

    Made activity and attendee dto's

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -6,17 +6,18 @@ using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<ActivityDto>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, ActivityDto>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -24,9 +25,12 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<ActivityDto> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities
+                    .Include(x => x.UserActivities)
+                    .ThenInclude(x => x.AppUser)
+                    .SingleOrDefaultAsync(x => x.Id == request.Id);
 
                 if (activity == null)
                     throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
